94f8367d50b68b6b6d2d23a76e413bce239f572a
Glitch (nebula-fine-wool) <none>
2025-05-23 18:44:49 +0000

    🎶➕ Updated with Glitch

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+function aprekins() {
+  let x=parseInt(document.getElementById("x").value);
+  let y=parseInt(document.getElementById("y").value);
+  let z=x+y;
+  console.log("Pirmā vērtība:+ x ");
+  console.log("Otrā vērtība:+ y ");
+  console.log("Rezultāts:+ z ");
+  //alert("Rezultats ir"+z);
+}
+
 window.onload= function() {
   zimetuzcanva();
   none alert("Rītdien Oranžais Brīdinājums, sola negaisu ar krusu");
@@ -15,12 +25,12 @@ function aplis() {
   konteksts.arc(110, 330, 100, 0, 2 * Math.PI);
   konteksts.fillStyle = "red";
   konteksts.fill();
-  konteksts.lineWidth =15;
+  konteksts.lineWidth =15; 
   konteksts.strokeStyle = "yellow";
   konteksts.stroke();
 }
   //teksts
-function tristuris () {
+function tristuris() {
   const kanva = document.getElementById("zimejums"); //Iegūt kanvu pēc ID
   const konteksts = kanva.getContext("2d") // iegūt 2d zīmēšanu
   konteksts.font = "30px Arial";
@@ -28,7 +38,7 @@ function tristuris () {
   konteksts.fillText("Sveicieni!", 50, 50);
 }
   //līnija
-function linija () {
+function linija() {
   const kanva = document.getElementById("zimejums"); //Iegūt kanvu pēc ID
   const konteksts = kanva.getContext("2d") // iegūt 2d zīmēšanu  
   konteksts.beginPath();
